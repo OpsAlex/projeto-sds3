e5ea905dac2fc8b7592d98e66f7042d13aa83286
OpsAlex <alexbezerra50@hotmail.com>
2021-05-09 11:12:37 -0300

    First request

--- frontend/src/components/DonutCharts/index.tsx
+++ frontend/src/components/DonutCharts/index.tsx
@@ -1,13 +1,35 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 // https://apexcharts.com/   nesse site é possivel pegar varios tipos de graficos
 const DonutCharts = () => {
 
-const mockData = {
+    //Forma Errada
+    let chartData : ChartData = { labels: [], series: [] };
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then(response => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+        
+        chartData = {labels: myLabels, series: mySeries};
+        console.log(response.data);
+
+    });
+
+
+
+/* const mockData = {
     series: [477138, 499928, 444867, 220426, 473088],
     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
 }
-
+*/
 const options = {
     legend: {
         show: true
@@ -18,8 +40,8 @@ const options = {
 
 return (
     <Chart
-        options={{ ...options, labels: mockData.labels }}
-        series={mockData.series}
+        options={{ ...options, labels: chartData.labels }}
+        series={chartData.series}
         type="donut"
         height="240"
     />
